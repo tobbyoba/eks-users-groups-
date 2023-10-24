81b3b2c728e32e705f3856dea35837bb61801c72
tobbyoba <tobbyoba86@gmail.com>
2023-10-24 17:36:33 -0500

    updated output.tf

--- eks-groups/output.tf
+++ eks-groups/output.tf
@@ -2,3 +2,8 @@ output "password" {
   value = aws_iam_user_login_profile.Admin_user.*.encrypted_password
   ###password | base64 --decode | keybase pgp decrypt
 }
+
+output "password" {
+  value = aws_iam_user_login_profile.dev_user.*.encrypted_password
+  ###password | base64 --decode | keybase pgp decrypt
+}
